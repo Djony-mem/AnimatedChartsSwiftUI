571d342e494ed618a751d2d82c2e1a7675a0cdd8
Бруйко Евгения <bruyko.e@gmail.com>
2021-02-09 16:06:32 +0300

    Initial Commit

---
+++ AnimatedChartsSwiftUI/AnimatedChartsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedChartsSwiftUIApp.swift
+//  AnimatedChartsSwiftUI
+//
+//  Created by Brubrusha on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedChartsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
